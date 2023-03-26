32ebe67ffa287003a7f0eb38edd3f1ce7c787f75
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2023-03-27 00:35:55 +0530

    Initial project configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-4xl bg-red-400">Hello there</h1>
+      <Button>Anything</Button>
+    </div>
+  );
+}
+
+export default App;
