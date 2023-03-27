815d96d012f34c3ce615f215a72ee59232d6f322
Piyush Pradhan <piyushpradhan3.14@gmail.com>
2023-03-27 11:17:57 +0530

    Checking pre-commit hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 function App() {
   return (
-    <div className="w-screen h-full">
+    <div className="w-screen h-full flex flex-col justify-center items-center">
       <h1 className="text-4xl">Hello there</h1>
     </div>
   );
